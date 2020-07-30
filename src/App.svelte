afeea3b7a6946e8cd06874da0b767f6f3358785d
Paul Brugger <paul@brugger.com>
2020-07-30 11:43:22 -0600

    oops forgot to add files

---
+++ src/App.svelte
@@ -0,0 +1,47 @@
+<script>
+  import Space from "./Space.svelte";
+</script>
+
+<style>
+  main {
+    width: 475px;
+    margin: 0 auto;
+    height: 1000px;
+  }
+  .row {
+    display: flex;
+  }
+  button {
+    width: 100%;
+    margin-top: 20px;
+    border-radius: 0;
+    background-color: lightblue;
+    font-size: 30px;
+    cursor: pointer;
+  }
+  button:hover {
+    outline: none;
+  }
+</style>
+
+<main>
+  <h1>Tic Tac Toe</h1>
+  <h2>Number of people playing:</h2>
+  <h2>Player X</h2>
+  <div class="row">
+    <Space space="X" />
+    <Space space="X" />
+    <Space space="X" />
+  </div>
+  <div class="row">
+    <Space space="X" />
+    <Space space="X" />
+    <Space space="X" />
+  </div>
+  <div class="row">
+    <Space space="X" />
+    <Space space="X" />
+    <Space space="X" />
+  </div>
+  <button>New Game</button>
+</main>
---
+++ src/Space.svelte
@@ -0,0 +1,18 @@
+<script>
+  export let space = "";
+</script>
+
+<style>
+  .space {
+    border: solid 1px gray;
+    height: 125px;
+    width: 125px;
+    flex: 1;
+    font-size: 75px;
+    text-align: center;
+    line-height: 125px;
+    cursor: pointer;
+  }
+</style>
+
+<div class="space">{space}</div>
